4b1cb414b5ae834a155f4dedda813ce10cb13ec0
Krzysiek <krzysztof.kierylowski@gmail.com>
2025-01-14 21:26:16 +0100

    created feature for changing layers colors & layer depth

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -13,6 +13,7 @@ import { LiveObject } from '@liveblocks/client'
 import { LayerPreview } from './layer-preview'
 import { toast } from 'sonner'
 import { SelectionBox } from './selection-box'
+import { SelectionTools } from './selection-tools'
 interface CanvasProps {
 	boardId: string
 }
@@ -240,6 +241,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 				undo={history.undo}
 				redo={history.redo}
 			/>
+			<SelectionTools camera={camera} setLastUsedColor={setLastUsedColor}/>
 			<svg
 				className="h-[100vh] w-[100vw]"
 				onWheel={onWheel}
